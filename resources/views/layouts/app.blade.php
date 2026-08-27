7744232981d552c6dd293cfa78e4d4c8d0d646f2
janak37 <janakdangi37@gmail.com>
2026-08-27 10:29:51 +0545

    Use @yield for page title instead of unset variable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Taskflow' }}</title>
+    <title>@yield('title', 'Taskflow')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="min-h-screen bg-stone-100 text-slate-900 antialiased">
