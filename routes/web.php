871549c4a6fb10e72a50b8128ed556e22567e235
janak37 <janakdangi37@gmail.com>
2026-08-30 16:18:17 +0545

    Add guest/auth middleware groups and auth routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\AuthController;
 
 Route::redirect('/', '/tasks');
-Route::resource('tasks', TaskController::class);
-Route::patch('tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
+
+Route::middleware('guest')->group(function () {
+    Route::get('register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::get('login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('login', [AuthController::class, 'login']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::resource('tasks', TaskController::class);
+    Route::patch('tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
+});
